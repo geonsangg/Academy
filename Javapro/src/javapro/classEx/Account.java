b0ac35d4410d243b6fb3f46250ae8dd3afb8295f
geonsangg <rjstkdd@naver.com>
2022-12-28 20:47:19 +0900

    class 1 (은행계좌클래스 객체생성)

---
+++ Javapro/src/javapro/classEx/Account.java
@@ -0,0 +1,23 @@
+package javapro.classEx;
+
+//은행계좌 클래스
+public class Account {
+	// 필드
+	String accountNo; // 계좌번호
+	String ownerName; // 예금주
+	int balance; // 잔액
+
+	// 예금한다 메서드
+	void deposit(int amount) {
+		balance += amount;
+	}
+
+	// 인출한다 메서드
+	int withdraw(int amount) {
+		if (balance < amount) {
+			return 0;
+		}
+		balance -= amount;
+		return amount;
+	}
+}
