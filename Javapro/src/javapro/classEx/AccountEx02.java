dc57dfdf9d287ddde56b9e3b675db8bce558f1b1
geonsangg <rjstkdd@naver.com>
2022-12-29 17:06:37 +0900

    class (생성자 있는 은행계좌)

---
+++ Javapro/src/javapro/classEx/AccountEx02.java
@@ -0,0 +1,24 @@
+package javapro.classEx;
+
+public class AccountEx02 {
+
+	public static void main(String[] args) {
+		                  //new 생성자(  ) 값을 초기에 저장해놓는것.
+		Account geonsang = new Account("111-222-33333", "이건상" , 1000); 
+		Account leegeon = new Account("111-333-444444", "리건", 0);
+		geonsang.deposit(6000);
+		leegeon.deposit(10000);
+		geonsang.withdraw(2000);
+		printAccount(geonsang);
+		printAccount(leegeon);
+	}
+
+	//예금정보 출력 메서드
+	public static void printAccount(Account obj) {
+		System.out.println("계좌번호 : " + obj.accountNo);
+		System.out.println("예금주 : " + obj.ownerName);
+		System.out.println("잔액 : " + obj.balance);
+		System.out.println("----------------------------");
+	}
+	
+}
