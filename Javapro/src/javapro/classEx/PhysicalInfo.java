432beace429ab49b6c17f516c7f0d859aea5c3a0
geonsangg <rjstkdd@naver.com>
2022-12-29 19:08:44 +0900

    class (메서드 오버로딩) + (신체정보 객체생성)

---
+++ Javapro/src/javapro/classEx/PhysicalInfo.java
@@ -0,0 +1,31 @@
+package javapro.classEx;
+//신체정보 클래스
+public class PhysicalInfo {
+	//필드
+	String name; // 이름
+	int age; //나이
+	float height, weight; //키,몸무게
+	
+	//생성자
+	public PhysicalInfo(String name, int age, float height, float weight ) {
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+	
+	//메서드
+	void update(int age, float height, float weight) {
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+	}
+	//메서드 오버로딩 = 같은 메서드를 여러개 만드는 것 (단.매개변수 개수가 다르거나 타입이 달라야함) 
+	void update(int age, float height) {
+		this.age = age;
+		this.height = height;
+	}
+	void update(int age) {
+		this.age = age;
+	}
+}
