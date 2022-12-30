507a3eac4f907b28280365fe55dce614a88306da
geonsangg <rjstkdd@naver.com>
2022-12-30 21:09:29 +0900

    class (메서드 오버로딩)+ (getter) + (신체정보 객체생성)

--- Javapro/src/javapro/classEx/PhysicalInfo.java
+++ Javapro/src/javapro/classEx/PhysicalInfo.java
@@ -2,9 +2,10 @@ package javapro.classEx;
 //신체정보 클래스
 public class PhysicalInfo {
 	//필드
-	String name; // 이름
+	private String name; // 이름
 	int age; //나이
 	float height, weight; //키,몸무게
+	
 	
 	//생성자
 	public PhysicalInfo(String name, int age, float height, float weight ) {
@@ -28,4 +29,8 @@ public class PhysicalInfo {
 	void update(int age) {
 		this.age = age;
 	}
+	public String getName() { //getter = private(접근제한자)인 필드를 외부에서 읽게하기위해서.(읽기전용)
+		return name;
+	}
+	
 }
