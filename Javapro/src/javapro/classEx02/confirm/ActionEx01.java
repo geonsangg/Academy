df7b42376328426542c109c89fc2ffa5a22ff93d
geonsangg <rjstkdd@naver.com>
2023-01-05 21:06:57 +0900

    중첩 클래스 (확인문제 5)

--- Javapro/src/javapro/classEx02/confirm/ActionEx01.java
+++ Javapro/src/javapro/classEx02/confirm/ActionEx01.java
@@ -3,15 +3,15 @@ package javapro.classEx02.confirm;
 public class ActionEx01 {
 
 	public static void main(String[] args) {
-		Action action = new Action() {
+		Action action = new Action() { //익명 객체
 			
 			@Override
 			public void work() {
-				System.out.println("복사를 합니다.");
+				System.out.println("복사를 합니다."); 
 				
 			}
 		};
-		action.work();
+		action.work(); //메서드실행
 	}
 
 }
